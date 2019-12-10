d9346b20675122ee08b6cbe1291963718f6d4e73
Francisco Jose Chaves <pachopepe@gmail.com>
2019-12-10 13:45:41 -0500

    Command line process

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,45 @@
 module Main where
 
-import           OpenApi
+import OpenApi3.FileUtils (formatOnly, process)
+import System.Console.GetOpt
+import Data.List
+import System.Environment
+import System.Exit
+import System.IO
+
+data Flags = FormatOnly
+           | Help
+    deriving (Eq,Ord,Enum,Show,Bounded)
+
+flags =
+       [Option ['f'] ["format-only"]       (NoArg FormatOnly)
+            "Format only without refactoring"
+       ,Option ['h'] ["help"]       (NoArg Help)
+            "Print this help message"
+       ]
+
+parse :: [String] -> IO ([Flags], [String])
+parse argv = 
+    case getOpt Permute flags argv of
+        (args,fs,[]) -> 
+            if length fs /= 2 || Help `elem` args
+                then do hPutStrLn stderr (usageInfo header flags)
+                        exitSuccess
+                else return (nub args, fs)
+
+        (_,_,errs)      -> do
+            hPutStrLn stderr (concat errs ++ usageInfo header flags)
+            exitWith (ExitFailure 1)
+
+        where header = "Usage: openapi-refactor [-fh] <input-file> <output-file>"
+
 
 main :: IO ()
-main = refactor
+main = do
+    (as, [inputFile, outputFile]) <- getArgs >>= parse
+    if FormatOnly `elem` as
+    then
+        formatOnly inputFile outputFile 
+    else
+        process inputFile outputFile 
+
--- src/OpenApi3/FileUtils.hs
+++ src/OpenApi3/FileUtils.hs
@@ -4,13 +4,14 @@ import           Data.Yaml
 import           Data.Yaml.Pretty
 import qualified Data.ByteString               as B
 import qualified Data.Map.Lazy                 as M
-import qualified Data.Set                 as S
+import qualified Data.Set                      as S
 import           OpenApi3.Models                ( OpenApiObject )
-import           OpenApi3.Refactor
+import           OpenApi3.Refactor              ( factorizeOpenApi, Dict(..) )
 import           Control.Monad.State.Lazy
 
 config = setConfCompare compare . setConfDropNull True $ defConfig
 
+saveToDisk :: FilePath -> OpenApiObject -> IO ()
 saveToDisk outputFile contents =
     B.writeFile outputFile . encodePretty config $ (contents :: OpenApiObject)
 
@@ -19,3 +20,17 @@ loadFromDisk = decodeFileEither
 
 refactor :: OpenApiObject -> OpenApiObject
 refactor o = evalState (factorizeOpenApi o) (Dict (M.empty,S.empty) (M.empty,S.empty) (M.empty,S.empty))
+
+process :: FilePath -> FilePath -> IO ()
+process inputFile outputFile = do
+    eContents <- loadFromDisk inputFile 
+    case eContents of
+      Left err -> error (show err)
+      Right contents -> saveToDisk outputFile . refactor $ contents 
+
+formatOnly :: FilePath -> FilePath -> IO ()
+formatOnly inputFile outputFile = do
+    eContents <- loadFromDisk inputFile 
+    case eContents of
+      Left err -> error (show err)
+      Right contents -> saveToDisk outputFile contents 
